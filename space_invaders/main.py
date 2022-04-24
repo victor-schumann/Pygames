c19c04a499501f9b1c7d8bb8599324a925742faa
victor-schumann <schumann@disroot.org>
2022-04-23 22:43:22 -0300

    Add screen resolution

--- space_invaders/main.py
+++ space_invaders/main.py
@@ -4,7 +4,14 @@ import os
 import time
 import random
 
+# Screen Config
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Friendly Space Game")
+
+
 # Image Loading
+bg = pygame.image.load(os.path.join("assets", "background_black.png"))
 
 # Space Ships
 ENEMY_SHIP_V = pygame.image.load(os.path.join("assets", "pixel_ship_vertical.png"))
@@ -14,10 +21,13 @@ ENEMY_SHIP_BOSS_2 = pygame.image.load(os.path.join("assets", "pixel_ship_boss_2.
 HERO_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_hero.png"))
 
 # Lasers
-RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
-
-# Background
-bg = pygame.image.load(os.path.join("assets", "background_black.png"))
+RED_LASER_LINE = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+RED_LASER_BALL = pygame.image.load(os.path.join("assets", "pixel_laser_ball_red.png"))
+GREEN_LASER_LINE = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+GREEN_LASER_BALL = pygame.image.load(os.path.join("assets", "pixel_laser_ball_green.png"))
+BLUE_LASER_LINE = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+BLUE_LASER_BALL = pygame.image.load(os.path.join("assets", "pixel_laser_ball_blue.png"))
+YELLOW_LASER_LINE = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+YELLOW_LASER_BALL = pygame.image.load(os.path.join("assets", "pixel_laser_ball_yellow.png"))
+BOSS_LASER_LINE = pygame.image.load(os.path.join("assets", "pixel_laser_vertical_1.png"))
+BOSS_LASER_BALL = pygame.image.load(os.path.join("assets", "pixel_laser_vertical_2.png"))
